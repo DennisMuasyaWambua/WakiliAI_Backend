7eccb0e556fb2ab3a3dd04d5ab61f0e37aa9e51a
sammy ocharo <obanyisammy@gmail.com>
2026-04-27 09:53:55 +0300

    fix collect static

--- wakiliai/settings.py
+++ wakiliai/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from decouple import config
 import dj_database_url
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 
 
