d2e603e501797d9652908a1b8c91005571b3a84a
sammy ocharo <obanyisammy@gmail.com>
2026-04-28 15:11:52 +0300

    add cors

--- wakiliai/settings.py
+++ wakiliai/settings.py
@@ -26,13 +26,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'authentication.apps.AuthenticationConfig',
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",
 ]
 
+# CORS Settings
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_HEADERS = [
+    "authorization",
+    "content-type",
+    "accept",
+    "origin",
+    "x-requested-with",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
